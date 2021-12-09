9b4d3174193fab8a728e6280cecd9e5012952b04
alizhaaan <alizhan.zhapan@nu.edu.kz>
2021-12-09 21:22:56 +0600

    Clerkpage updated

--- src/pages/ClerkPage/ClerkPage.js
+++ src/pages/ClerkPage/ClerkPage.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import "./ClerkPage.css"
 
+
+
 function ClerkPage() {
     const [details, setDetails] = useState({res_id: "", guest_id: "", hotel_id: "", r_number: "", check_in: "", check_out: ""});
 
@@ -12,31 +14,32 @@ function ClerkPage() {
             <h3>Create a reservation</h3>
             <div className="form-group">
                     <label>Reservation ID: </label>
-                    <input type="number" name="res_id" id="res_id" onChange={e => setDetails({...details, res_id: e.target.value})} value={details.res_id} />
+                    <input type="number" className="inp" name="res_id" id="res_id" onChange={e => setDetails({...details, res_id: e.target.value})} value={details.res_id} />
             </div>
             <div className="form-group">
                     <label>Guest ID: </label>
-                    <input type="number" name="guest_id" id="guest_id" onChange={e => setDetails({...details, guest_id: e.target.value})} value={details.guest_id} />
+                    <input type="number" className="inp" name="guest_id" id="guest_id" onChange={e => setDetails({...details, guest_id: e.target.value})} value={details.guest_id} />
             </div>
             <div className="form-group">
                     <label>Hotel ID: </label>
-                    <input type="number" name="hotel_id" id="hotel_id" onChange={e => setDetails({...details, hotel_id: e.target.value})} value={details.hotel_id} />
+                    <input type="number" className="inp" name="hotel_id" id="hotel_id" onChange={e => setDetails({...details, hotel_id: e.target.value})} value={details.hotel_id} />
             </div>
             <div className="form-group">
                     <label>Room Number: </label>
-                    <input type="number" name="r_number" id="r_number" onChange={e => setDetails({...details, r_number: e.target.value})} value={details.r_number} />
+                    <input type="number" className="inp" name="r_number" id="r_number" onChange={e => setDetails({...details, r_number: e.target.value})} value={details.r_number} />
             </div>
             <div className="form-group">
                     <label>Check IN date: </label>
-                    <input type="date" name="check_in" id="check_in" onChange={e => setDetails({...details, check_in: e.target.value})} value={details.check_in} />
+                    <input type="date" className="inp" name="check_in" id="check_in" onChange={e => setDetails({...details, check_in: e.target.value})} value={details.check_in} />
             </div>
             <div className="form-group">
                     <label>Check OUT date: </label>
-                    <input type="date" name="check_out" id="check_out" onChange={e => setDetails({...details, check_out: e.target.value})} value={details.check_out} />
+                    <input type="date" className="inp" name="check_out" id="check_out" onChange={e => setDetails({...details, check_out: e.target.value})} value={details.check_out} />
             </div>
-            <input type="submit" value="Create Reservation" className="button" />
+            <input type="submit" value="Create" className="button1" />
         </form>
         <br/><br/><br/>
+        <h4>This is the list of the reservations made by guests:</h4>
         <p className="reservations">This is this reservation <input type="submit" value="Edit" className="but" /> <input type="submit" value="Delete" className="but"/> </p>
         </div>
     )
